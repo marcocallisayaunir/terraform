2b7e7c4b946be05ac7befba8a22b764f0ae3c8cf
Marco Callisaya <marcoemilio.callisaya@unir.net>
2024-10-14 17:39:21 -0400

    update app services

--- ocr/azure/modules/app_service/main.tf
+++ ocr/azure/modules/app_service/main.tf
@@ -3,13 +3,13 @@ resource "azurerm_service_plan" "plan" {
   name                = var.service_plane_name
   resource_group_name = var.resource_group_name
   location            = var.location
-  os_type             = "Linux"
+  os_type             = "Windows"
   sku_name            = "P1v3"
   tags                = var.tags
 }
 
 # 
-resource "azurerm_linux_web_app" "api" {
+resource "azurerm_windows_web_app" "api" {
   for_each            = var.apps
   name                = each.value.app_service_name
   resource_group_name = var.resource_group_name
